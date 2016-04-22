36829d84fa5eb73625a75e740bf6c4b97993e936
Robert Wascher <rwascher9@gmail.com>
2016-04-22 06:14:45 -0500

    More Setup Work
    More work adding in code to setup the application

--- Game Engine/GameEngine/Engine.cpp
+++ Game Engine/GameEngine/Engine.cpp
@@ -1,13 +1,36 @@
 #include "Engine.h"
 #include <d3d11.h>
+#include <dxgi.h>
+#include <assert.h>
+
+#define ReleaseCOM(x) { if(x){ x->Release(); x = 0; } }
+namespace
+{
+	Engine* app = 0;
+}
 
 Engine::Engine()
-	: bRun( true )
+	: bRun( true ), mEnable4xMsaa( false ), mMainWnd( 0 ), mClientWidth( 800 ), mClientHeight( 600 ), md3dDriverType( D3D_DRIVER_TYPE_HARDWARE ), 
+	mCaption( "Game Engine" ),  msaaQuality(0), Device(0), Context(0), SwapChain(0), DepthStencil(0), RenderTargetView(0), DepthStencilView(0)
 {
+	ZeroMemory( &Viewport, sizeof( D3D11_VIEWPORT ) );
+	app = this;
 }
 
 Engine::~Engine()
 {
+	ReleaseCOM(RenderTargetView);
+	ReleaseCOM(DepthStencilView);
+	ReleaseCOM(SwapChain);
+	ReleaseCOM(DepthStencil);
+
+	if( Context )
+	{
+		Context->ClearState();
+	}
+
+	ReleaseCOM(Context);
+	ReleaseCOM(Device);
 }
 
 void Engine::run()
@@ -53,8 +76,6 @@ void Engine::PreLoad()
 	};
 
 	D3D_FEATURE_LEVEL featureLevel;
-	ID3D11Device* Device;
-	ID3D11DeviceContext* Context;
 	HRESULT res = D3D11CreateDevice( 0, D3D_DRIVER_TYPE_HARDWARE, 0, 0, featureLevels, 0, D3D11_SDK_VERSION, &Device, &featureLevel , &Context );
 
 	if( FAILED(res) )
@@ -62,27 +83,51 @@ void Engine::PreLoad()
 		MessageBox( 0, "D3D11CreateDevice Failed\n", 0, 0 );
 	}
 
-	DXGI_SWAP_CHAIN_DESC swapChainDesc = {0};
-	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-	swapChainDesc.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+	Device->CheckMultisampleQualityLevels( DXGI_FORMAT_R8G8B8A8_UNORM, 4, &msaaQuality );
+	assert( msaaQuality > 0 );
+
+	DXGI_SWAP_CHAIN_DESC swapChainDesc;
 	swapChainDesc.BufferDesc.Width = 0;
 	swapChainDesc.BufferDesc.Height = 0;
-	swapChainDesc.BufferDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM; //check later whether it should be RGBA
+	swapChainDesc.BufferDesc.RefreshRate.Denominator = 1;
+	swapChainDesc.BufferDesc.RefreshRate.Numerator = 60;
+	swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapChainDesc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+	swapChainDesc.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+
+	if( mEnable4xMsaa )
+	{
+		swapChainDesc.SampleDesc.Count = 4;
+		swapChainDesc.SampleDesc.Quality = msaaQuality - 1;
+	}
+	else
+	{
+		swapChainDesc.SampleDesc.Count = 1;
+		swapChainDesc.SampleDesc.Quality = 0;
+	}
+
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
 	swapChainDesc.BufferCount = 2;
+	swapChainDesc.Windowed = true;
+	swapChainDesc.OutputWindow = mMainWnd;
 	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL;
 
-	IDXGIDevice1* dxgiDevice = (IDXGIDevice1*)Device;
-	dxgiDevice->SetMaximumFrameLatency(1);
+	IDXGIDevice1* dxgiDevice = 0;
+	Device->QueryInterface( __uuidof(IDXGIDevice), (void**)&dxgiDevice );
+	
+	IDXGIAdapter* dxgiAdapter = 0;
+	dxgiDevice->GetParent( __uuidof(IDXGIAdapter), (void**)&dxgiAdapter );
 
-	IDXGIAdapter* dxgiAdapter;
-	dxgiDevice->GetAdapter( &dxgiAdapter );
-
-	IDXGIFactory1* dxgiFactory;
-	dxgiAdapter->GetParent( IID_PPV_ARGS(&dxgiFactory) );
+	IDXGIFactory* dxgiFactory = 0;
+	dxgiAdapter->GetParent( __uuidof(IDXGIFactory), (void**)&dxgiFactory );
 
 	IDXGISwapChain* swapChain;
 
 	dxgiFactory->CreateSwapChain(Device, &swapChainDesc, &swapChain );
+
+	ReleaseCOM(dxgiDevice);
+	ReleaseCOM(dxgiAdapter);
+	ReleaseCOM(dxgiFactory);
 }
 
 void Engine::ClearBuffers()
--- Game Engine/GameEngine/Engine.h
+++ Game Engine/GameEngine/Engine.h
@@ -1,6 +1,8 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 
+#include <d3d11.h>
+
 class Engine
 {
 public:
@@ -23,6 +25,23 @@ private:
 
 protected:
 	bool bRun;
+
+	HINSTANCE mApp;
+	HWND	  mMainWnd;
+	UINT	  msaaQuality;
+	ID3D11Device* Device;
+	ID3D11DeviceContext* Context;
+	IDXGISwapChain* SwapChain;
+	ID3D11Texture2D* DepthStencil;
+	ID3D11RenderTargetView* RenderTargetView;
+	ID3D11DepthStencilView* DepthStencilView;
+	D3D11_VIEWPORT Viewport;
+
+	char* mCaption;
+	D3D_DRIVER_TYPE md3dDriverType;
+	int mClientWidth;
+	int mClientHeight;
+	bool mEnable4xMsaa;
 };
 
 #endif
