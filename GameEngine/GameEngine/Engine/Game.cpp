690d2649e31857604b0c038f80d5d56e5a81121f
Robert Wascher <rwascher9@gmail.com>
2016-06-12 07:03:51 -0500

    forgot a file

--- GameEngine/GameEngine/Engine/Game.cpp
+++ GameEngine/GameEngine/Engine/Game.cpp
@@ -5,13 +5,16 @@
 #include "ShaderManager.h"
 #include "ModelManager.h"
 #include "MaterialManager.h"
-#include "ConstantBuffers.h"
+#include "GraphicsObject.h"
+#include "GameObject.h"
 #include "GraphicsEngine.h"
+
+GameObject* go = 0;
 
 Game::Game( HINSTANCE hInstance )
 	: Engine( hInstance )
 {
-	bgColor = Vect( 1.0f, 1.0f, 1.0f, 1.0f );
+	bgColor = Vect( 0.3f, 0.3f, 0.3f, 1.0f );
 	FullTimer.tic();
 	intervalTimer.tic();
 }
@@ -33,62 +36,28 @@ void Game::LoadContent()
 	MaterialManager::createBaseMat( Material_ID::Base_Solid, ShaderManager::get( Shader_ID::Base ) );
 
 	ModelManager::createCube( Model_ID::Cube_Model );
-}
 
-Matrix Rot = Matrix (IDENTITY );
+	GraphicsObject* go1 = new GraphicsObject( Model_ID::Cube_Model, Material_ID::Base_Solid );
+	go = new GameObject( GameObject_ID::Cube, go1 );
+	go->setStaticScale( Matrix( SCALE, .1f, .1f, .1f ) );
+	go->setStaticTrans( Matrix( TRANS, .5f, .5f, .5f ) );
+}
 
 void Game::Update()
 {
 	intervalTimer.toc();
 	intervalTimer.tic();
 
-	static int cnt = 0;
-	static Time prev = Time( TIME_ZERO );
-	static Time dif  = Time( TIME_ZERO );
 	Time current = FullTimer.toc();
-	dif += current - prev;
-	prev = current;
-
-	if( dif >=  Time( TIME_ONE_SECOND ) )
-	{
-		dif = Time ( TIME_ZERO );
-		printf( "Tic%d\n", cnt );
-		cnt++;
-	}
-
-	bgColor[x] = 1.0f / (float)cnt;
-	bgColor[y] = 1.0f / (float)cnt;
-	bgColor[z] = 1.0f / (float)cnt;
-
-	Rot *= Matrix(ROT_XYZ, .0005f, .0005f, .0005f );
+	
+	Matrix id = Matrix( IDENTITY );
+	go->update( current, id, id );
 }
 
 void Game::Draw()
 {
-	Shader* shade = MaterialManager::get( Material_ID::Base_Solid )->activate();
-	Context->VSSetShader( shade->getVertexShader(), nullptr, 0 );
-	Context->PSSetShader( shade->getPixelShader(), nullptr, 0 );
-
-	baseBuffer buf;
-	buf.model = Matrix( SCALE, .1f, .1f, .1f )  * Rot * Matrix( TRANS, .5f, .5f, .5f );
-	buf.proj = Matrix( IDENTITY );
-	buf.view = Matrix( IDENTITY );
-
-	ID3D11Buffer* cBuf = shade->getConstBuffer();
-	Context->UpdateSubresource( cBuf, 0, nullptr, &buf, 0, 0 );
-	Context->VSSetConstantBuffers(0, 1, &cBuf );
-
-	Model* model = ModelManager::get( Model_ID::Cube_Model );
-	Context->IASetVertexBuffers( 0, 1, model->getVertexBuffer(), model->getStride(), model->getOffset() ); 
-	Context->IASetIndexBuffer( model->getIndexBuffer(), DXGI_FORMAT_R32_UINT, 0 ); 
-
-	Context->IASetInputLayout( shade->getVertexLayout() );
-    Context->IASetPrimitiveTopology( D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST );
-
-	Context->DrawIndexed( 36, 0, 0 ); 
-
-	HRESULT res = GraphicsEngine::getSwapChain()->Present ( 0, 0 );
-	assert( res == S_OK );
+	Matrix id = Matrix( IDENTITY );
+	go->draw( id );
 }
 
 void Game::ClearBuffers()
@@ -101,4 +70,5 @@ void Game::UnloadContent()
 	MaterialManager::clear();
 	ShaderManager::clear();
 	ModelManager::clear();
+	delete go;
 }
